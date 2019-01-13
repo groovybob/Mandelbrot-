164d134c1430812300253da7b34889cda1ec3a0f
Alex Dale <alex.dale78@gmail.com>
2019-01-13 21:03:44 +0100

    did that yeally work?

--- Mandelbrot3.py
+++ Mandelbrot3.py
@@ -1,9 +1,24 @@
-import pygame
-import sys
+import numpy as np
+import matplotlib.pyplot as plt
+from numba import autojit
 
-pygame.init()
+def Mandelbrot(Re, Im, max_iter ):
+    c = complex(Re,Im)
+    z = 0.0j
 
+    for i in range (max_iter):
+        z = z**2 + c
+        if (z.real*z.real + z.imag*z.imag) >= 4:
+            return i
 
-#pygame.draw.rect(screen, color, (x,y,width,height), thickness)
+    return max_iter
+columns = 2000
+rows = 2000
 
-screen = pygame.display.set_mode((200,200))
+result = np.zeros([rows, columns])
+for row_index, Re in enumerate(np.linspace(-2,1,num=rows)):
+    for columns_index, Im in enumerate(np.linspace(-1,1,num=columns)):
+        result[row_index, columns_index] = Mandelbrot(Re,Im,100)
+
+plt.figure(dpi=100)
+plt.imshow(result.T, cmap = 'hot', interpolation='bilinear', extent=[])
